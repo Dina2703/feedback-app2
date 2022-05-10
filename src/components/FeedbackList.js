b6b34c40743e1615867e7c4755bc2fceaa5d0cc7
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-05-10 16:23:23 -0400

    A bit animation with 'framer motion'  package

--- src/components/FeedbackList.js
+++ src/components/FeedbackList.js
@@ -1,17 +1,41 @@
 import PropTypes from "prop-types";
+import { motion, AnimatePresence } from "framer-motion";
 import FeedbackItem from "./FeedbackItem";
 
 function FeedbackList({ feedback, handleDelete }) {
   if (!feedback || feedback.length === 0) {
     return <h2 style={{ color: "green" }}>No Feedback Yet</h2>;
   }
+
+  //Animated feedback-list
   return (
     <div className="feedback-list">
-      {feedback.map((item) => (
-        <FeedbackItem item={item} key={item.id} handleDelete={handleDelete} />
-      ))}
+      <AnimatePresence>
+        {feedback.map((item) => (
+          <motion.div
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <FeedbackItem
+              item={item}
+              key={item.id}
+              handleDelete={handleDelete}
+            />
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
   );
+
+  // return (
+  //   <div className="feedback-list">
+  //     {feedback.map((item) => (
+  //       <FeedbackItem item={item} key={item.id} handleDelete={handleDelete} />
+  //     ))}
+  //   </div>
+  // );
 }
 
 FeedbackList.propTypes = {
