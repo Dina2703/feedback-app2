05de34fa1bb39382153a9021c1ea4377216c6279
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-05-10 13:18:41 -0400

    FeedbackForm input, custom Button component

---
+++ src/components/FeedbackForm.js
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import Button from "./shared/Button";
+import Card from "./shared/Card";
+
+function FeedbackForm() {
+  const [text, setText] = useState("");
+  const handleTextChange = (e) => {
+    setText(e.target.value);
+  };
+  return (
+    <Card>
+      <form>
+        <h2>How would you rate your service with us?</h2>
+        <div className="input-group">
+          <input
+            type="text"
+            placeholder="write your review"
+            value={text}
+            onChange={handleTextChange}
+          />
+          <Button type="submit" version="secondary">
+            send
+          </Button>
+        </div>
+      </form>
+    </Card>
+  );
+}
+
+export default FeedbackForm;
